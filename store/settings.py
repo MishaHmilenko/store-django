6103735262929a5d6f0da735e484b98bc7ca3645
Misha <290819allo@gmail.com>
2023-11-20 10:02:05 +0200

    change settings.py

--- store/settings.py
+++ store/settings.py
@@ -170,11 +170,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 if DEBUG:
-    STATICFILES_DIRS = (
-        BASE_DIR / 'static',
-    )
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
 else:
-    STATIC_ROOT = BASE_DIR / 'static'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
